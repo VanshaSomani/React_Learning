0d1f632ebdf8639a0e17c0aec7c52f4c1ab8414a
VanshaSomani <vanshasomani0@gmail.com>
2024-02-24 09:57:19 +0530

    aboutus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { AppContext } from "./context";
+import { Header } from './components/Header';
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import { AboutUs } from './components/AboutUs';
 
 function App() {
+  var title = "Royal technosoft";
+  var addresss = "Ahmedabad";
+  axios.defaults.baseURL = "https://node5.onrender.com";
   return (
     <div className="App">
+      <Header headertitle={title}/>
       <AppContext.Provider>
-        
+      <Routes>
+        <Route path='/aboutus' element={<AboutUs/>}></Route>
+      </Routes>
       </AppContext.Provider>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <div className='header'>
+                <h1>Header</h1>
+            </div>
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href='#'>
+                                Home
+                            </a>
+                        </li>
+                        <li class="navbar-item active">
+                            <Link class="nav-link" to="/aboutus">
+                                AboutUs
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
 }
